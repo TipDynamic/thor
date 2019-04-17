e8e22f2eb09b6458c56230dc3517e936b6989986
Your Name <you@example.com>
2019-04-17 22:35:19 +0800

    add freetype support

--- examples/thor_demo.cpp
+++ examples/thor_demo.cpp
@@ -16,42 +16,89 @@
 #include "opencv2/opencv.hpp"
 #include "./include/dog.h"
 #include "thor/app_license.h"
+#include "thor/vis.h"
 
 using namespace std;
 using namespace thor;
 using namespace cv;
 
-int main(int argc, char **argv)
+
+static int ToWchar(char* &src, wchar_t* &dest, const char *locale = "zh_CN.utf8")
 {
-
-    cout << "Welcome!\n";
-    cout << colors::bold << colors::yellow << "thor library\n"
-         << colors::reset;
-
-    if (thor::os::isdir("../"))
-    {
-        cout << colors::green << "and this is a " << colors::bold << colors::yellow << "dir.\n";
-        vector<string> allFiles;
-        allFiles = thor::os::list_files("../", true);
-        cout << "list all files under it:\n";
-        for (auto f : allFiles)
-        {
-            cout << f << endl;
-        }
-    }
-    cout << colors::reset;
-
-    string p = "/media/netac/a.mp4";
-    string suffix = thor::os::suffix(p);
-    cout << "suffix: " << suffix << endl;
-
-    thor::Log(kInfo, "this a log test.");
-
-    vector<float> a = {4.5, 6.5, 6, 7, 9};
-    vector<float> b = {4.1, 3.5, 4, 7, 9};
-
-    float res = thor::math::euclidean(a, b);
-    cout << "euclidean res: " << to_string(res) << endl;
-
+  if (src == NULL) {
+    dest = NULL;
     return 0;
+  }
+
+  // 根据环境变量设置locale
+  setlocale(LC_CTYPE, locale);
+
+  // 得到转化为需要的宽字符大小
+  int w_size = mbstowcs(NULL, src, 0) + 1;
+
+  // w_size = 0 说明mbstowcs返回值为-1。即在运行过程中遇到了非法字符(很有可能使locale
+  // 没有设置正确)
+  if (w_size == 0) {
+    dest = NULL;
+    return -1;
+  }
+
+  //wcout << "w_size" << w_size << endl;
+  dest = new wchar_t[w_size];
+  if (!dest) {
+    return -1;
+  }
+
+  int ret = mbstowcs(dest, src, strlen(src)+1);
+  if (ret <= 0) {
+    return -1;
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+
+  cout << "Welcome!\n";
+  cout << colors::bold << colors::yellow << "thor library\n"
+       << colors::reset;
+
+  if (thor::os::isdir("../")) {
+    cout << colors::green << "and this is a " << colors::bold << colors::yellow << "dir.\n";
+    vector<string> allFiles;
+    allFiles = thor::os::list_files("../", true);
+    cout << "list all files under it:\n";
+    for (auto f : allFiles) {
+      cout << f << endl;
+    }
+  }
+  cout << colors::reset;
+
+  string p = "/media/netac/a.mp4";
+  string suffix = thor::os::suffix(p);
+  cout << "suffix: " << suffix << endl;
+
+  thor::Log(kInfo, "this a log test.");
+
+  vector<float> a = {4.5, 6.5, 6, 7, 9};
+  vector<float> b = {4.1, 3.5, 4, 7, 9};
+
+  float res = thor::math::euclidean(a, b);
+  cout << "euclidean res: " << to_string(res) << endl;
+
+  cv::Mat img = cv::imread("../1.png");
+  if (!img.data || img.channels() != 3) {
+    fprintf(stderr, "read image fail\n");
+    return -1;
+  }
+
+  thor::vis::CvxText cvText("../FZSSJW.TTF"); //指定字体
+  cv::Scalar size1{38, 0.5, 0.1, 0}; // (字体大小, 无效的, 字符间距, 无效的 }
+  cvText.setFont(nullptr, &size1, nullptr, 0);
+  char* str = (char *)"你好，世界!";
+  wchar_t *w_str;
+  ToWchar(str,w_str);
+  cvText.putText(img, w_str, cv::Point(50, 100), cv::Scalar(255, 0, 255));
+  cv::imshow("demo", img);
+  cv::waitKey(0);
+  return 0;
 }
--- include/vis.h
+++ include/vis.h
@@ -5,7 +5,6 @@
 #ifndef CAO_VIS_H
 #define CAO_VIS_H
 
-
 /**
  *  Vision module in cao
  *  the module provide some utility methods to draw bboxes
@@ -23,28 +22,94 @@
 #include <map>
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
-
+#include <freetype2/freetype/config/ftheader.h>
+#include FT_FREETYPE_H
+#include <opencv2/opencv.hpp>
+#include <wchar.h>
+#include <assert.h>
+#include <locale.h>
+#include <ctype.h>
+#include <cmath>
 
 using namespace cv;
 
-namespace thor {
+namespace thor
+{
 
-   namespace vis{
-       struct RGB{
-           double r, g, b;
-       };
+namespace vis
+{
 
-       struct RGBA{
-           int r, g, b, a;
-       };
+class CvxText
+{
+  public:
+    CvxText(const char *freeType);
+    virtual ~CvxText();
+    void getFont(int *type, cv::Scalar *size = nullptr, bool *underline = nullptr, float *diaphaneity = nullptr);
+    void setFont(int *type, cv::Scalar *size = nullptr, bool *underline = nullptr, float *diaphaneity = nullptr);
 
-       thor::vis::RGBA gen_unique_color(int idx, double hue_step, float alpha);
+    void restoreFont();
 
-       void hsv2rgb(float &r, float &g, float &b, int h, float s, double v);
+    int putText(cv::Mat &img, char *text, cv::Point pos);
+    int putText(cv::Mat &img, const wchar_t *text, cv::Point pos);
+    int putText(cv::Mat &img, const char *text, cv::Point pos, cv::Scalar color);
+    int putText(cv::Mat &img, const wchar_t *text, cv::Point pos, cv::Scalar color);
 
-       cv::Mat createAlpha(cv::Mat& src);
-       int addAlpha(cv::Mat& src, cv::Mat& dst, cv::Mat& alpha);
-   }
-}
+    inline static int ToWchar(char *&src, wchar_t *&dest, const char *locale = "zh_CN.utf8")
+    {
+        if (src == NULL)
+        {
+            dest = NULL;
+            return 0;
+        }
+        setlocale(LC_CTYPE, locale);
+        int w_size = mbstowcs(NULL, src, 0) + 1;
+        if (w_size == 0)
+        {
+            dest = NULL;
+            return -1;
+        }
+        dest = new wchar_t[w_size];
+        if (!dest)
+        {
+            return -1;
+        }
+        int ret = mbstowcs(dest, src, strlen(src) + 1);
+        if (ret <= 0)
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+  private:
+    CvxText &operator=(const CvxText &);
+    void putWChar(cv::Mat &img, wchar_t wc, cv::Point &pos, cv::Scalar color);
+
+    FT_Library m_library; // 字库
+    FT_Face m_face;       // 字体
+    int m_fontType;
+    cv::Scalar m_fontSize;
+    bool m_fontUnderline;
+    float m_fontDiaphaneity;
+};
+
+struct RGB
+{
+    double r, g, b;
+};
+
+struct RGBA
+{
+    int r, g, b, a;
+};
+
+thor::vis::RGBA gen_unique_color(int idx, double hue_step, float alpha);
+
+void hsv2rgb(float &r, float &g, float &b, int h, float s, double v);
+
+cv::Mat createAlpha(cv::Mat &src);
+int addAlpha(cv::Mat &src, cv::Mat &dst, cv::Mat &alpha);
+} // namespace vis
+} // namespace thor
 
 #endif //CAO_VIS_H
